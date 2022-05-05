7e50975811f533380d164dc963df86313bf8be56
Sebabin <sebastiancrespin279@gmail.com>
2022-05-04 23:22:13 -0600

    have footer correctly recognizing if a todo is checked completed or not as well as being able to update colors and delete an item

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import TodoListItem from './TodoListItem';
 
-const selectTodos = state => state.todos;
+const selectTodoIds = state => state.todos.map(todo => todo.id);
 
 const TodoList = () => {
     //  useSelector automatically subscribes to the Redux store for us! That way, any time an action is dispatched, it will call its selector function again right away.
     // If the value returned by the selector changes from the last time it ran, useSelector will force our component to re-render with the new data. All we have to do is call useSelector()
-  const todos = useSelector(selectTodos);
+  const todoIds = useSelector(selectTodoIds, shallowEqual);
 
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />
   })
 
   return <ul className="todo-list">{renderedListItems}</ul>
