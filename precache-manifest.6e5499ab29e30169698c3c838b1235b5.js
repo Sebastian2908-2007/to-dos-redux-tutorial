ae33700749dbe3d7adc3923c20f294f8a49b6b68
Sebabin <sebastiancrespin279@gmail.com>
2022-05-12 15:55:59 -0600

    Updates

---
+++ precache-manifest.6e5499ab29e30169698c3c838b1235b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3224fe08acb3b079d1a3e5ada573fbca",
+    "url": "/to-dos-redux-tutorial/index.html"
+  },
+  {
+    "revision": "382d0005c9f9ce4c2ceb",
+    "url": "/to-dos-redux-tutorial/static/css/main.a2a9b2a6.chunk.css"
+  },
+  {
+    "revision": "7a9533ddd220cb1c1267",
+    "url": "/to-dos-redux-tutorial/static/js/2.3b9776bc.chunk.js"
+  },
+  {
+    "revision": "e32ca7c165361cea986caa4a88235453",
+    "url": "/to-dos-redux-tutorial/static/js/2.3b9776bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382d0005c9f9ce4c2ceb",
+    "url": "/to-dos-redux-tutorial/static/js/main.8b317929.chunk.js"
+  },
+  {
+    "revision": "c54bb0e522675eab3cd6",
+    "url": "/to-dos-redux-tutorial/static/js/runtime-main.ee185d75.js"
+  },
+  {
+    "revision": "dddb6154d71cf6b6afc56ab2420837a7",
+    "url": "/to-dos-redux-tutorial/static/media/times-solid.dddb6154.svg"
+  }
+]);
